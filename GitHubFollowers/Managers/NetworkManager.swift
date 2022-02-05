8dbf991f6f0bc716a3576a9a9b19af080e656c2f
ALeX <alexdeoliveira.poli@gmail.com>
2022-02-05 15:10:33 -0300

    Downloading & Caching Avatar Images

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Alex Oliveira on 04/02/2022.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
 
     private init() { }
 
