b2ec9fc688249dfde3e15989ad117482843a807e
ALeX <alexdeoliveira.poli@gmail.com>
2022-02-07 09:28:02 -0300

    Modal Presentation & Passing Data

---
+++ GitHubFollowers/Screens/UserInfoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfoViewController.swift
+//  GitHubFollowers
+//
+//  Created by Alex Oliveira on 07/02/2022.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
+        navigationItem.rightBarButtonItem = doneButton
+
+        print(username!)
+    }
+
+    @objc func dismissViewController() {
+        dismiss(animated: true)
+    }
+}
