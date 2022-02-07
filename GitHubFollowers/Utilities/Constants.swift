ce06aff990adb0c5f8a8bdc5b93821dc831194ef
ALeX <alexdeoliveira.poli@gmail.com>
2022-02-07 14:06:40 -0300

    UserInfoViewController Cleanup & Setup

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Alex Oliveira on 07/02/2022.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
