23fa6cfa876872d273ac01e42510a3d04d524cc2
ALeX <alexdeoliveira.poli@gmail.com>
2022-02-09 18:39:12 -0300

    Search, Pagination, Empty State, Constants & More

--- GitHubFollowers/CustomViews/Views/GFEmptyStateView.swift
+++ GitHubFollowers/CustomViews/Views/GFEmptyStateView.swift
@@ -26,25 +26,40 @@ class GFEmptyStateView: UIView {
     }
 
     private func configure() {
+        configureMessageLabel()
+        configureLogoImageView()
+    }
+
+    private func configureMessageLabel() {
         addSubview(messageLabel)
-        addSubview(logoImageView)
 
         messageLabel.numberOfLines = 3
         messageLabel.textColor = .secondaryLabel
 
-        logoImageView.image = UIImage(named: "empty-state-logo")
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        let messageLabelCenterYConstant: CGFloat = (DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed) ? -100 : -150
 
         NSLayoutConstraint.activate([
-            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: messageLabelCenterYConstant),
             messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
             messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
-            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
 
+    private func configureLogoImageView() {
+        addSubview(logoImageView)
+
+        logoImageView.image = Images.emptyStateLogo
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        let logoImageViewTrailingConstant: CGFloat = (DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed) ? 130 : 170
+        let logoImageViewBottomConstant: CGFloat = (DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed) ? 30 : 40
+
+        NSLayoutConstraint.activate([
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, constant: 1.3),
-            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: logoImageViewTrailingConstant),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: logoImageViewBottomConstant)
         ])
     }
 }
