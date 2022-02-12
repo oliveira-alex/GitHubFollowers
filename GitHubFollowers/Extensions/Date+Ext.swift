0884d17e4dc29abec2222081cc817af0ca83926e
ALeX <alexdeoliveira.poli@gmail.com>
2022-02-12 07:17:55 -0300

    New Date Formatter

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -11,11 +11,4 @@ extension Date {
     func convertToMonthYearFormat() -> String {
         return formatted(.dateTime.month().year())
     }
-
-//    func convertToMonthYearFormat() -> String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "MMM yyyy"
-//
-//        return dateFormatter.string(from: self)
-//    }
 }
